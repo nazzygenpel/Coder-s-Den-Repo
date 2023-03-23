5c50541a448ae7063417b3ac43fa661dc9b0b78d
Chinaza Glory <nazzyglory@gmail.com>
2023-03-23 16:14:16 +0100

    initial commit

--- Day5Exercises.js
+++ Day5Exercises.js
@@ -179,10 +179,16 @@ let fullStack = frontEnd.concat('backEnd')
 
     const ages = [19, 22, 19, 24, 20, 25, 26, 24, 25, 24]
 
-        //Sort the array and find the min and max age
-         let age=ages.sort
-        console.log (ages)
-        //Find the median age(one middle item or two middle items divided by two)
+         //Sort the array and find the min and max age
+         let sorted = ages.sort();
+         const min = sorted.slice(0, 1);
+         const max = sorted.slice(ages.length-1);
+         console.log (min , max)
+
+        //Find the median age(one middle item or two middle items divided by two)   
+let len= sorted.legth/2;
+    console.log(sorted)
+
         //Find the average age(all items divided by number of items)
         //Find the range of the ages(max minus min)
         //Compare the value of (min - average) and (max - average), use abs() method 1.Slice the first ten countries from the countries array
